22b3ecf2ac43f2eabfc9742d2dd910e0f32a7b50
Shaunak Chakraborty <31863612+ShaunakChakraborty@users.noreply.github.com>
2022-03-27 23:40:49 +0530

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import {onMount} from 'svelte';
+	import {createScene} from "./scene.js";
+
+	let webgl;
+
+	onMount(() => {
+		createScene(webgl)
+	});
+</script>
+
+<canvas bind:this={webgl}></canvas>
+
+<style>
+	:global(body){
+		margin: 0;
+		padding: 0;
+	}
+</style>
